e0194271afbac97723bdf5a9774fff8799e8c21a
Cristian Betivu <cbe@languagewire.com>
2022-11-09 23:10:59 +0200

    Merge branch feature/openapi

--- apps/esports-api/src/main.ts
+++ apps/esports-api/src/main.ts
@@ -1,5 +1,6 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -7,10 +8,24 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+  setupOpenAPI(app);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+}
+
+function setupOpenAPI(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('ESports API')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
 }
 
 bootstrap();
